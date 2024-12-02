775c488b976809566fe6164345eb0e2dccb157a4
khanhh89 <khanhabcabc0000@gmail.com>
2024-12-02 19:46:44 +0700

    Update bt2.cpp

--- bt2.cpp
+++ bt2.cpp
@@ -1,23 +1,24 @@
 #include<stdio.h>
 int main(){
-	int arr[7]={98,63,789,65,32,1,0};
-	int size=sizeof(arr)/sizeof(int);
-	printf("Chua sap xep:\n");
+	int number[7]={98,63,789,65,32,1,0};
+		int size=sizeof(number)/sizeof(int);
+	printf("Mang chua sap xep la:\n");
 	for(int i=0;i<size;i++){
-		printf("%d\t",arr[i]);
+		printf("%d\t",number[i]);
 	}
-	for(int i=0; i<size;i++){
-		int min=arr[i];
-		for(int j=i+1;j<size;j++){
-			if(arr[j]<min){
-				min= arr[j]; 
-				arr[j]=arr[i];
-				arr[i]=min;
+	for(int i=0;i<size;i++){
+		for(int j=0;j<size-1-i;j++){
+			if(number[j+1]<number[j]){
+			int temp = number[j];
+			number[j]=number[j+1];
+			number[j+1]=temp;
 			}
 		}
 	}
-	printf("\nDa xap xep:\n");
+	printf("\nMang da sap xep la:\n");
 	for(int i=0;i<size;i++){
-		printf("%d\t",arr[i]);
+		printf("%d\t",number[i]);
+		
 }
+return 0;
 }
